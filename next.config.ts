49914b5d9a86d10af5ae303f0ace168ff888786d
KarenGuyumjyan <karen.guyumjyan99@gmail.com>
2026-04-03 00:18:13 +0400

    Fix deployment issue

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['bcryptjs', '@prisma/client', 'prisma'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
